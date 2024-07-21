d054f148637872720d6b08be25e1583922605e2c
Ikrar Bagaskara <ikrar@Ikrars-iMac-Pro.local>
2024-07-21 11:49:18 +0700

    implemented authentication

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 const ClientError = require('./exceptions/ClientError');
 
 const albums = require('./api/albums');
@@ -14,10 +15,16 @@ const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
 
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
+
 const init = async () => {
   const albumService = new AlbumsService();
   const songService = new SongsService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
 
   const app = Hapi.server({
     port: process.env.PORT,
@@ -27,6 +34,28 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  await app.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  app.auth.strategy('musicapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await app.register(
@@ -50,6 +79,15 @@ const init = async () => {
         options: {
           service: usersService,
           validator: UsersValidator,
+        },
+      },
+      {
+        plugin: authentications,
+        options: {
+          authenticationsService,
+          usersService,
+          tokenManager: TokenManager,
+          validator: AuthenticationsValidator,
         },
       },
     ],

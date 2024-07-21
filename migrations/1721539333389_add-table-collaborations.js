1b8188663b110927c4ea71e71b74b9f3e8e71d27
Ikrar Bagaskara <ikrar@Ikrars-iMac-Pro.local>
2024-07-22 06:47:07 +0700

    fix duplicate constraint

--- migrations/1721539333389_add-table-collaborations.js
+++ migrations/1721539333389_add-table-collaborations.js
@@ -19,9 +19,6 @@ exports.up = (pgm) => {
   });
 
   pgm.addConstraint('collaborations', 'unique_playlist_id_and_user_id', 'UNIQUE(playlist_id, user_id)');
-
-  pgm.addConstraint('collaborations', 'fk_collaborations.playlist_id_playlists.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(playlist_id) ON DELETE CASCADE');
-  pgm.addConstraint('collaborations', 'fk_collaborations.user_id_users.id', 'FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE');
 };
 
 exports.down = (pgm) => {

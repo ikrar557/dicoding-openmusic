b6d92c4f4fa9762d82c78b4dc6d79decb0ae90dc
Ikrar Bagaskara <ikrar@Ikrars-iMac-Pro.local>
2024-07-28 23:42:18 +0700

    add consumer or nodemailer

---
+++ consumer/Consumer.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const amqp = require('amqplib');
+const PlaylistsService = require('./PlaylistsService');
+const SongService = require('./SongService');
+const MailSender = require('./MailSender');
+const Listener = require('./Listener');
+
+const init = async () => {
+  const playlistsService = new PlaylistsService();
+  const songsService = new SongService();
+  const mailSender = new MailSender();
+  const listener = new Listener(playlistsService, songsService, mailSender);
+
+  const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
+  const channel = await connection.createChannel();
+
+  await channel.assertQueue('export:playlist', {
+    durable: true,
+  });
+
+  channel.consume('export:playlist', listener.listen, { noAck: true });
+};
+
+init();
---
+++ consumer/Listener.js
@@ -0,0 +1,38 @@
+const autoBind = require('auto-bind');
+
+class Listener {
+  constructor(playlistsService, songsService, mailSender) {
+    this._playlistsService = playlistsService;
+    this._songsService = songsService;
+    this._mailSender = mailSender;
+
+    autoBind(this);
+  }
+
+  async listen(message) {
+    try {
+      const messageContent = message.content.toString();
+      const { playlist, targetEmail } = JSON.parse(messageContent);
+      const { id: playlistId } = playlist;
+
+      const fetchedPlaylist = await this._playlistsService.getSongsByPlaylist(playlistId);
+      const songs = await this._songsService.getSongsByPlaylist(playlistId);
+
+      const playlistSongs = {
+        playlist: {
+          ...fetchedPlaylist,
+          songs,
+        },
+      };
+
+      const prettyJson = JSON.stringify(playlistSongs, null, 2);
+
+      const result = await this._mailSender.sendEmail(targetEmail, prettyJson);
+      console.log('Email sent result:', result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+module.exports = Listener;
